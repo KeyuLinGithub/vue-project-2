98e9fbf3791af741df9691a053594034542705f3
keyu Lin <mlchaellin222@gmail.com>
2018-06-28 09:20:56 +0800

    initial program

---
+++ src/views/common/Login.vue
@@ -0,0 +1,70 @@
+<template>
+	<div class="login">
+    	
+    	<div class="login-con">
+	        <Card :bordered="false">
+	            <p slot="title">
+                    <Icon type="log-in" style="margin-right: 6px;"></Icon>欢迎登录
+	            </p>
+	            <div class="form-con">
+					<form autocomplete="off" class="ivu-form ivu-form-label-right">
+						<div class="ivu-form-item ivu-form-item-required">
+							<!---->
+							<div class="ivu-form-item-content">
+								<div class="ivu-input-wrapper ivu-input-type ivu-input-group ivu-input-group-with-prepend">
+									<div class="ivu-input-group-prepend" style="">
+										<span>
+											<i class="ivu-icon ivu-icon-person" style="font-size: 16px;"></i>
+										</span>
+									</div>
+									<!----><i class="ivu-icon ivu-icon-load-c ivu-load-loop ivu-input-icon ivu-input-icon-validate"></i> 
+									<input autocomplete="off" spellcheck="false" type="text" placeholder="请输入用户名" class="ivu-input">
+									<!---->
+								</div>
+								<!---->
+							</div>
+						</div>
+						<div class="ivu-form-item ivu-form-item-required">
+							<!---->
+							<div class="ivu-form-item-content">
+								<div class="ivu-input-wrapper ivu-input-type ivu-input-group ivu-input-group-with-prepend">
+									<div class="ivu-input-group-prepend" style="">
+										<span>
+											<i class="ivu-icon ivu-icon-locked" style="font-size: 14px;"></i>
+										</span>
+									</div>
+									<!----><i class="ivu-icon ivu-icon-load-c ivu-load-loop ivu-input-icon ivu-input-icon-validate"></i> 
+									<input autocomplete="off" spellcheck="false" type="password" placeholder="请输入密码" class="ivu-input">
+									<!---->
+								</div>
+								<!---->
+							</div>
+						</div>
+						<div class="ivu-form-item">
+							<!---->
+							<div class="ivu-form-item-content"><button type="button" class="ivu-btn ivu-btn-primary ivu-btn-long"><!----> <!----> <span>登录</span></button>
+								<!---->
+							</div>
+						</div>
+					</form>
+					<p class="login-tip">输入任意用户名和密码即可</p>
+				</div>
+	        </Card>
+	    </div>
+    </div>
+    
+</template>
+
+<script>
+var Main = {
+    
+}
+
+var Component = Vue.extend(Main)
+new Component().$mount('#app')
+
+
+
+</script>
+</body>
+</html>
---
+++ src/views/page/table_2.vue
@@ -0,0 +1,167 @@
+// table-expand.vue
+<style scoped>
+    .expand-row{
+        margin-bottom: 16px;
+    }
+</style>
+<template>
+	
+	<Button type="primary">增加</Button>
+    <Button type="ghost">删除</Button>
+    
+    
+    
+    <Input type="text" placeholder="用户名" style="width: 300px"></Input>
+    <Input type="text" placeholder="手机号" style="width: 300px"></Input>
+    <Input type="text" placeholder="邮箱" style="width: 300px"></Input>
+    <Select  style="width:200px">
+        <Option >职位</Option>
+    </Select>
+    <Select  style="width:200px">
+        <Option >省市</Option>
+    </Select>
+    
+    <Row>
+        <Col span="12">
+            <DatePicker type="date" placeholder="Select date" style="width: 200px"></DatePicker>
+        </Col>
+        <Col span="12">
+            <DatePicker type="daterange" placement="bottom-end" placeholder="Select date" style="width: 200px"></DatePicker>
+        </Col>
+    </Row>
+    
+    <Select  style="width:200px">
+        <Option >用户状态</Option>
+    </Select>
+    
+    <Button type="primary">查询</Button>
+    <Button type="ghost">重置</Button>
+    
+	
+    <div>
+        <Row class="expand-row">
+            <Col span="8">
+                <span class="expand-key">Job: </span>
+                <span class="expand-value">{{ row.job }}</span>
+            </Col>
+            <Col span="8">
+                <span class="expand-key">Interest: </span>
+                <span class="expand-value">{{ row.interest }}</span>
+            </Col>
+            <Col span="8">
+                <span class="expand-key">Birthday: </span>
+                <span class="expand-value">{{ row.birthday }}</span>
+            </Col>
+        </Row>
+        <Row>
+            <Col span="8">
+                <span class="expand-key">Favorite book: </span>
+                <span class="expand-value">《{{ row.book }}》</span>
+            </Col>
+            <Col span="8">
+                <span class="expand-key">Favorite movie: </span>
+                <span class="expand-value">{{ row.movie }}</span>
+            </Col>
+            <Col span="8">
+                <span class="expand-key">Favorite music: </span>
+                <span class="expand-value">{{ row.music }}</span>
+            </Col>
+        </Row>
+    </div>
+    
+    <Page :total="100"></Page>
+    
+</template>
+<script>
+    export default {
+        props: {
+            row: Object
+        }
+    };
+</script>
+
+// table.vue
+<template>
+    <Table :columns="columns10" :data="data9"></Table>
+</template>
+<script>
+    import expandRow from './table-expand.vue';
+    export default {
+        components: { expandRow },
+        data () {
+            return {
+                columns10: [
+                    {
+                        type: 'expand',
+                        width: 50,
+                        render: (h, params) => {
+                            return h(expandRow, {
+                                props: {
+                                    row: params.row
+                                }
+                            })
+                        }
+                    },
+                    {
+                        title: 'Name',
+                        key: 'name'
+                    },
+                    {
+                        title: 'Age',
+                        key: 'age'
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
+                    }
+                ],
+                data9: [
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        job: 'Data engineer',
+                        interest: 'badminton',
+                        birthday: '1991-05-14',
+                        book: 'Steve Jobs',
+                        movie: 'The Prestige',
+                        music: 'I Cry'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 25,
+                        address: 'London No. 1 Lake Park',
+                        job: 'Data Scientist',
+                        interest: 'volleyball',
+                        birthday: '1989-03-18',
+                        book: 'My Struggle',
+                        movie: 'Roman Holiday',
+                        music: 'My Heart Will Go On'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        job: 'Data Product Manager',
+                        interest: 'tennis',
+                        birthday: '1992-01-31',
+                        book: 'Win',
+                        movie: 'Jobs',
+                        music: 'Don’t Cry'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        job: 'Data Analyst',
+                        interest: 'snooker',
+                        birthday: '1988-7-25',
+                        book: 'A Dream in Red Mansions',
+                        movie: 'A Chinese Ghost Story',
+                        music: 'actor'
+                    }
+                ]
+            }
+        }
+    }
+</script>
